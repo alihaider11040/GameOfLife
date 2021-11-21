60ea01d2a77b695faad4e345a7910a861f18bcc1
Muhammad Ahmad <ahmaddddddddddkhan@gmail.com>
2021-11-22 00:09:58 +0500

    Removed extra classes Created DB interface Interfaces updated waiting for Grid Class

--- src/com/company/DB_DAL.java
+++ src/com/company/DB_DAL.java
@@ -1,5 +1,14 @@
 package com.company;
 
-public class DB_DAL {
+public class DB_DAL implements DB_interface {
 
+
+    @Override
+    public void SaveGrid(Grid grid) {
+        
+    }
+    @Override
+    public void LoadGrid() {
+
+    }
 }
--- src/com/company/DB_interface.java
+++ src/com/company/DB_interface.java
@@ -3,6 +3,4 @@ package com.company;
 public interface DB_interface {
     public void SaveGrid();
     public void LoadGrid();
-    public void DeleteGrid();
-    
 }
