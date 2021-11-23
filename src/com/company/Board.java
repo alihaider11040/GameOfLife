1d2b9fd39311f7c070893e0bace7bf571d427c16
Ali Haider <alihaider11040@gmail.com>
2021-11-24 00:03:39 +0500

    Board upated

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -2,5 +2,16 @@ package com.company;
 
 public class Board {
     // here we will add cell class
-    cell
+    int rows;
+    int cols;
+    cell[][] gameBoard=new cell[rows][cols];
+    boolean stateRun;
+    Board(){
+
+    }
+    Board(int r, int c){
+        rows=r;
+        cols=c;
+        stateRun=false;
+    }
 }
