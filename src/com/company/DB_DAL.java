a17dff9e6901df2bc0c79fec28f3ada5efd63ec6
Muhammad Ahmad <ahmaddddddddddkhan@gmail.com>
2021-11-24 01:09:12 +0500

    DB DAL implemented Lexicon SP implemented Delete SPs Implemented

--- src/com/company/DB_DAL.java
+++ src/com/company/DB_DAL.java
@@ -13,7 +13,7 @@ public class DB_DAL implements DB_interface {
             Connection connection = DriverManager.getConnection(DB_url,USER_NAME,Pass );
             CallableStatement save_record_procedure = connection.prepareCall("{call SAVE_INTO_GAME_DETAILS(?,?,?,?,?)}");
             save_record_procedure.setInt(1,1);
-            save_record_procedure.setString(2,"shariq_Nigga");
+            save_record_procedure.setString(2,"Cinderellaman");
             save_record_procedure.setInt(3,200);
             save_record_procedure.setInt(4,10);
             save_record_procedure.setInt(5,1000);
@@ -35,13 +35,39 @@ public class DB_DAL implements DB_interface {
         game_details.execute();
         connection.close();
     }
+
+    @Override
+    public void delete_game_details() throws SQLException {
+        Connection connection = DriverManager.getConnection(DB_url,USER_NAME,Pass);
+        CallableStatement delete_game_details= connection.prepareCall("{call DELETE_GAME_DETAILS(?)}");
+        delete_game_details.setInt(1,1);
+        delete_game_details.execute();
+        connection.close();
+    }
+
+    @Override
+    public void delete_game_record_details() throws SQLException {
+        Connection connection = DriverManager.getConnection(DB_url,USER_NAME,Pass);
+        CallableStatement delete_game_record_details =connection.prepareCall("{call DELETE_GAME_RECORD_DETAILS}");
+        delete_game_record_details.setInt(1,1);
+        delete_game_record_details.execute();
+        connection.close();
+    }
+
     @Override
     public void GetLexicon() throws SQLException {
         Connection connection = DriverManager.getConnection(DB_url,USER_NAME,Pass);
+        //Statement haroon= connection.createStatement();
+        //ResultSet shariq= haroon.executeQuery("select * from Lexicon");
+        //while(shariq.next())
+        //{
+        //    shariq.getInt("Lexicon_ID");
+        //}
         CallableStatement get_lexicon = connection.prepareCall(("{call GET_LEXICON(?)}"));
         get_lexicon.setInt(1,1);
         get_lexicon.execute();
         connection.close();
     }
+
 
 }
--- src/com/company/DB_interface.java
+++ src/com/company/DB_interface.java
@@ -5,5 +5,8 @@ import java.sql.SQLException;
 public interface DB_interface {
     public void SaveGrid();
     public void LoadGrid() throws SQLException;
+    public void delete_game_details() throws SQLException;
+    public void delete_game_record_details() throws SQLException;
     public void GetLexicon() throws SQLException;
+
 }
