24fcaab74a1b6d345ae2c50eed92abb43a9f0b16
ZAINAB DOGAR <l190995@lhr.nu.edu.pk>
2021-11-26 05:23:08 -0800

    SaveGrid Done

--- src/com/company/Filing_DAL.java
+++ src/com/company/Filing_DAL.java
@@ -8,15 +8,24 @@ import java.util.Scanner;
 public class Filing_DAL implements DB_interface
 {
     @Override
-    public void SaveGrid(Board obj) {
+    public void SaveGrid(int id ,Board obj)
+    {
         try
         {
             FileWriter write = new FileWriter("src/filename1.txt");
             cell[][] board = obj.getGameBoard();
-            for (int i = 0; i < obj.rows; i++) {
-                for (int j = 0; j < obj.cols; j++) {
-
+            for (int i = 0; i < obj.rows; i++)
+            {
+                for (int j = 0; j < obj.cols; j++)
+                {
+                    write.write(String.valueOf(id));
+                    // space
+                    write.write(String.valueOf(i));
+                    // space
+                    write.write(String.valueOf(j));
+                    // space
                     write.write(String.valueOf(board[i][j].aliveStatus));
+                    // end line
                 }
             }
 
@@ -28,19 +37,21 @@ public class Filing_DAL implements DB_interface
     }
 
     @Override
-    public void LoadGrid(int Grid_ID) throws SQLException {
+    public Board LoadGrid( int Grid_ID) throws SQLException
+    {
 
-    }
-
-
-
-    @Override
-    public void delete_saved_state(int Grid_ID) throws SQLException {
-
+        return LoadGrid(Grid_ID);
     }
 
     @Override
-    public void GetLexicon() throws SQLException {
+    public void delete_saved_state(int Grid_ID) throws SQLException
+    {
+
+    }
+
+    @Override
+    public void GetLexicon() throws SQLException
+    {
 
     }
 }
