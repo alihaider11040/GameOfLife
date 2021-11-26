490e1ef2ab6bf4022810734ffb2103bb09c598a4
ZAINAB DOGAR <l190995@lhr.nu.edu.pk>
2021-11-26 08:36:49 -0800

    FILING almost complete..

--- src/com/company/Filing/Filing_DAL.java
+++ src/com/company/Filing/Filing_DAL.java
@@ -4,9 +4,12 @@ import  com.company.BL.Board;
 import com.company.BL.cell;
 
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Filing_DAL implements DB_interface
 {
@@ -43,7 +46,27 @@ public class Filing_DAL implements DB_interface
     @Override
     public void LoadGrid(int Grid_ID) throws SQLException
     {
-        Board obj1 = new Board();
+        try
+        {
+            FileReader read = new FileReader("src/save_grid.txt");
+            Scanner reader = new Scanner(read);
+            Board object = new Board();
+
+            /*if(Grid_ID != 0)
+            {
+                while (reader.hasNextLine())
+                {
+                    String data = reader.nextLine();
+                    System.out.println(data);
+                }
+            }*/
+
+            reader.close();
+        } catch (FileNotFoundException e)
+        {
+            System.out.print("File not FOUND");
+            e.printStackTrace();
+        }
 
     }
 
@@ -53,12 +76,61 @@ public class Filing_DAL implements DB_interface
     }
 
     @Override
-    public void Save_game_details(Board obj) {
-
+    public void Save_game_details(Board obj)
+    {
+        try
+        {
+            FileWriter write1 = new FileWriter("src/save_details_grid.txt");
+            cell[][] board = obj.getGameBoard();
+            for (int i = 0; i < obj.rows; i++)
+            {
+                for (int j = 0; j < obj.cols; j++)
+                {
+                    write1.write(String.valueOf(board[i][j].aliveStatus));
+                    write1.write(" ");
+                }
+                write1.write("\n");
+                // end line
+            }
+            write1.close();
+        } catch (IOException e)
+        {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void load_game_details(int Game_ID) throws SQLException {
+    public void load_game_details(int Game_ID)
+    {
+        try
+        {
+            FileReader read1 = new FileReader("src/save_details_grid.txt");
+            Scanner reader1 = new Scanner(read1);
+            Board obj1 = new Board();
+            cell[][] board = obj1.getGameBoard();
 
+            /*if(Game_ID != 0)
+            {
+                while (reader1.hasNextLine())
+                {
+                    for (int i = 0; i < obj1.rows; i++)
+                    {
+                        for (int j = 0; j < obj1.cols; j++)
+                        {
+                            String data = reader1.nextLine();
+                            board[i][j].aliveStatus;
+                            System.out.println(data);
+                        }
+                    }
+                }
+            }*/
+
+            reader1.close();
+        } catch (FileNotFoundException e)
+        {
+            System.out.print("File not FOUND");
+            e.printStackTrace();
+        }
     }
 }
