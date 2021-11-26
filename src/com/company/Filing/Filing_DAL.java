6ad36c0b38765b3b8bc42aa7eefce386d217a99a
Ali Haider <alihaider11040@gmail.com>
2021-11-26 18:49:38 +0500

    Filing dal ready to test

--- src/com/company/Filing/Filing_DAL.java
+++ src/com/company/Filing/Filing_DAL.java
@@ -1,8 +1,7 @@
 package com.company.Filing;
-import com.company.BL.Board;
 import com.company.BL.DB_interface;
-import com.company.BL.cell;
-import com.company.BL.lexicon;
+import  com.company.BL.Board;
+
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,27 +10,9 @@ import java.sql.SQLException;
 public class Filing_DAL implements DB_interface
 {
 
-    public void SaveGrid(Board obj) {
-        try
-        {
-            FileWriter write = new FileWriter("src/filename1.txt");
-            cell[][] board = obj.getGameBoard();
-            for (int i = 0; i < obj.rows; i++) {
-                for (int j = 0; j < obj.cols; j++) {
-
-                    write.write(String.valueOf(board[i][j].aliveStatus));
-                }
-            }
-
-        } catch (IOException e)
-        {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
-
     @Override
     public void SaveGrid(int Game_ID, Board obj) {
+
 
     }
 
@@ -40,19 +21,18 @@ public class Filing_DAL implements DB_interface
 
     }
 
-
-
     @Override
     public void delete_saved_state(int Grid_ID) throws SQLException {
 
     }
 
     @Override
-    public void Save_game_details() {
+    public void Save_game_details(Board obj) {
 
     }
 
-    public void GetLexicon(lexicon obj) throws SQLException {
+    @Override
+    public void load_game_details(int Game_ID) throws SQLException {
 
     }
 }
