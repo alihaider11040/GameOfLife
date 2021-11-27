712a5a9c11078596c88e9ba5845ee90b19a86344
maliksidra774 <maliksidra774@gmail.com>
2021-11-27 15:42:26 +0500

    Button ID and style

--- src/com/company/UI/Main.java
+++ src/com/company/UI/Main.java
@@ -1,7 +1,10 @@
 package com.company.UI;
 
+
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -18,7 +21,7 @@ import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import org.w3c.dom.Text;
+import javafx.stage.Window;
 
 import javax.swing.*;
 
@@ -26,19 +29,31 @@ import javax.swing.*;
 public class Main extends Application {
 
 
+    Stage window;
     @Override
     public void start(Stage stage) throws Exception {
+        window=stage;
         int row=20;
         int col=50;
         Button bt1= new Button("Start");
         Button bt2= new Button("Stop");
         Button bt3= new Button("Next");
-        Button bt4= new Button("Lexicon");
-        Button bt5= new Button("Explaination");
-        Button bt6= new Button("Reset");
+        Button bt4= new Button("Explaination");
+        Button bt5= new Button("Reset");
         Slider s1= new Slider();
         Slider s2= new Slider();
 
+        ///// set ID to control buttons///
+        bt1.setId("play_button");
+        bt2.setId("play_button");
+        bt3.setId("play_button");
+        bt4.setId("play_button");
+        bt5.setId("play_button");
+
+///////////////////////////////////BUTTON STYLE////////////////////////////
+
+
+        ///////////////////////////BUTTON STYLE////////////
         GridPane g1= new GridPane();
         GridPane b1=new GridPane();
         GridPane finalGrid=new GridPane();
@@ -49,7 +64,7 @@ public class Main extends Application {
 
 
         // add title to stage
-        stage.setTitle("Game of Life");
+        window.setTitle("Game of Life");
         // add Image to icon
         //Image icon = new Image("i1.PNG");
         // stage.getIcons().add(icon);
@@ -57,7 +72,8 @@ public class Main extends Application {
         //  stage.setWidth(600);
         // stage.setHeight(600);
         // we are not able to change the stage size
-        stage.setResizable(true);
+        //stage.setResizable(true);
+        // stage.setMaximized(true);
         //  stage.setMinWidth(1024);
         // stage.setMaxWidth(1200);
         // below two line show the stage on 50,50 position on screen
@@ -69,40 +85,38 @@ public class Main extends Application {
         // stage.setFullScreenExitHint("Press f to exit full_screen mode");
         // stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("f"));
 
-        Text text=new Text();
-        text.setText("Game of Life");
-        text.setX(150);
-        text.setY(50);
-        // text.setFont(Font.font("papyrus",50));
-        text.setFont(Font.font("papyrus",FontWeight.findByWeight(50),FontPosture.REGULAR, 50 ));
-        text.setFill(Color.YELLOW);
-        //add text on scene
-        //  root.getChildrenUnmodifiable().add(text);
+
         //////////////////////// Grid /////////////////////////
 
         ///// Grid///////
         GridCells(row,col, g1);
         b1.add(bt1,0,0,1,1);
-        b1.add(bt2,1,0,1,1);
-        b1.add(bt3,2,0,1,1);
-        b1.add(bt4,3,0,1,1);
-        b1.add(bt5,4,0,1,1);
-        b1.add(bt6,5,0,1,1);
-        b1.add(s1,6,0,1,1);
-        b1.add(s2,7,0,1,1);
+        b1.add(bt2,10,0,1,5);
+        b1.add(bt3,20,0,1,5);
+        b1.add(bt4,30,0,1,5);
+        b1.add(bt5,40,0,1,5);
+        b1.add(s1,50,0,1,5);
+        b1.add(s2,60,0,1,5);
+        //b1.setStyle("-fx-background-color: pink");
         // g1.add(bt1,40,50,1,1);
         finalGrid.addRow(0,g1);
-        finalGrid.addRow(2,b1);
+        finalGrid.addRow(1,b1);
+        Scene scene= new Scene(finalGrid, Color.DARKGREY);
+        finalGrid.prefHeightProperty().bind(scene.heightProperty());
+        finalGrid.prefWidthProperty().bind(scene.widthProperty());
 
-        g1.setHgrow(g1, Priority.ALWAYS);
-        g1.setVgrow(g1,Priority.ALWAYS);
+        //g1.setHgrow(g1,Priority.ALWAYS);
+        //  g1.setVgrow(g1,Priority.ALWAYS);
 
-        Scene scene= new Scene(finalGrid,600,600, Color.DARKGREY);
+        //    stage.setResizable(false);
         //link sample.css file
-        // scene.getStylesheets().add(getClass().getResource("sample.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("sample.css").toExternalForm());
         // adding scene to stage
-        stage.setScene(scene);
-        stage.show();
+        window.setScene(scene);
+        // Image icon=new Image("i1.PNG");
+        //window.getIcons().add(icon);
+
+        window.show();
 
     }
 
@@ -114,12 +128,15 @@ public class Main extends Application {
             for (int j=0;j<col;j++)
             {
                 Button button=new Button();
+                ///////// set ID to Grid button//
+                button.setId("grid_button");
                 button.getStyleClass().add("emptybutton");
                 g1.add(button,j,i,1,1);
+                g1.setPadding(new Insets(1,1,10,1));
+                g1.setVgap(3);
+                g1.setHgap(3);
             }
         }
-
-
     }
 
 
