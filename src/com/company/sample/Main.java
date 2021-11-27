1f31434d9032da69cf1b118b95cf761642f6dc36
Ali Haider <alihaider11040@gmail.com>
2021-11-28 00:10:54 +0500

    UI working in our project mashallah

--- src/com/company/sample/Main.java
+++ src/com/company/sample/Main.java
@@ -1,14 +1,15 @@
-package com.company;
-
-
+package com.company.sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCombination;
@@ -31,32 +32,45 @@ public class Main extends Application {
 
     Stage window;
     @Override
-    public void start(Stage stage) throws Exception {
+    public  void start(Stage stage) throws Exception {
         window=stage;
-        int row=20;
-        int col=50;
+        int row=23;
+        int col=80;
         Button bt1= new Button("Start");
         Button bt2= new Button("Stop");
         Button bt3= new Button("Next");
-        Button bt4= new Button("Explaination");
+        Button bt4= new Button("Save");
         Button bt5= new Button("Reset");
         Slider s1= new Slider();
         Slider s2= new Slider();
 
         ///// set ID to control buttons///
-        bt1.setId("play_button");
-        bt2.setId("play_button");
-        bt3.setId("play_button");
-        bt4.setId("play_button");
-        bt5.setId("play_button");
+        bt1.setId("start");
+        bt2.setId("stop");
+        bt3.setId("next");
+        bt4.setId("save");
+        bt5.setId("reset");
 
-///////////////////////////////////BUTTON STYLE////////////////////////////
-
-
-        ///////////////////////////BUTTON STYLE////////////
+        bt1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("start");
+            }
+        });
+        bt2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("stop");
+            }
+        });
+        /////////////////////// Grid ///////////////
         GridPane g1= new GridPane();
         GridPane b1=new GridPane();
         GridPane finalGrid=new GridPane();
+        g1.setId("grid");
+
+
+        /////////////////////// Window title ////////////////
         //show stage
         // link sample.fxml file
         // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -88,40 +102,48 @@ public class Main extends Application {
 
         //////////////////////// Grid /////////////////////////
 
-        ///// Grid///////
+
+        ////////////// Button addition ////////////////////
+        b1.add(bt1,10,0,3,1);
+        b1.add(bt2,20,0,3,1);
+        b1.add(bt3,30,0,3,1);
+        b1.add(bt4,40,0,3,1);
+        b1.add(bt5,50,0,3,1);
+        b1.add(s1,60,0,3,1);
+        b1.add(s2,70,0,3,1);
+        b1.setHgap(3);
+
+        //////////////// Grid g1 add cells ////////////////
         GridCells(row,col, g1);
-        b1.add(bt1,0,0,1,1);
-        b1.add(bt2,10,0,1,5);
-        b1.add(bt3,20,0,1,5);
-        b1.add(bt4,30,0,1,5);
-        b1.add(bt5,40,0,1,5);
-        b1.add(s1,50,0,1,5);
-        b1.add(s2,60,0,1,5);
-        bt1.setLineSpacing(3);
-        //b1.setStyle("-fx-background-color: pink");
-        // g1.add(bt1,40,50,1,1);
+        //////////////////// Final Grid /////////////////////////
         finalGrid.addRow(0,g1);
         finalGrid.addRow(1,b1);
-        Scene scene= new Scene(finalGrid, Color.DARKGREY);
+        /////////////////////// Scene //////////////////////
+        Scene scene= new Scene(finalGrid,500,500, Color.DARKGREY);
         finalGrid.prefHeightProperty().bind(scene.heightProperty());
         finalGrid.prefWidthProperty().bind(scene.widthProperty());
 
-        //g1.setHgrow(g1,Priority.ALWAYS);
-        //  g1.setVgrow(g1,Priority.ALWAYS);
-
         //    stage.setResizable(false);
         //link sample.css file
+        //////////////////   CSS stylesheet //////////////////
         scene.getStylesheets().add(getClass().getResource("sample.css").toExternalForm());
-        // adding scene to stage
+        // adding scene to window
         window.setScene(scene);
         // Image icon=new Image("i1.PNG");
         //window.getIcons().add(icon);
 
+        ////////////// Show window/////////////
         window.show();
 
     }
 
+    //  @Override
+    // public  void handle(ActionEvent start)
+    // {
+    //    System.out.println("start");
+    //}
 
+    ///////////////// Grid cell function //////////////
     public void GridCells(int row, int col, GridPane g1)
     {
         for (int i=0; i<row;i++)
@@ -131,16 +153,14 @@ public class Main extends Application {
                 Button button=new Button();
                 ///////// set ID to Grid button//
                 button.setId("grid_button");
-                button.getStyleClass().add("emptybutton");
+                button.getStyleClass().add("empty_button");
                 g1.add(button,j,i,1,1);
-                g1.setPadding(new Insets(1,1,10,1));
+                g1.setPadding(new Insets(1,1,5,1));
                 g1.setVgap(3);
                 g1.setHgap(3);
             }
         }
     }
-
-
     public static void main(String[] args) {
         // belong to application class
         // static method that we inherit from parent application class
