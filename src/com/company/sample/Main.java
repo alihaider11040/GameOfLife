9d38d87460e721c34b8fff43ecf549c88e047261
SheikhAbdulMuizzTahir <sh.abdulmuizz@gmail.com>
2021-12-09 18:23:35 +0500

    Merge remote-tracking branch 'origin/master'

--- src/com/company/sample/Main.java
+++ src/com/company/sample/Main.java
@@ -1,4 +1,4 @@
-//package com.company.sample;
+/*//package com.company.sample;
 package com.company.sample;
 
 import java.io.BufferedReader;
@@ -283,7 +283,7 @@ public class Main extends Application {
                     public void handle(MouseEvent event) {
                         button.setStyle("-fx-background-color: #FFFF00; ");
                     }
-                });*/
+                });
 
             }
 
@@ -309,3 +309,57 @@ public class Main {
         dal.delete_saved_state(6);
     }
 */
+package com.company.sample;
+import com.company.BL.Board;
+import com.company.UI.terminalPrinting;
+import java.util.Scanner;
+import java.lang.Thread;
+
+
+public class Main {
+    public static void main(String args[])
+    {
+        terminalPrinting t1 = new terminalPrinting();
+        Board b1 = new Board(5,5);
+        b1.fillBoard();
+        while(true) {
+            t1.printTerminal(5, b1);
+            int x;
+            int y = 2500;
+            System.out.println("Press 1 to save game");
+            System.out.println("Press 2 to load saved game");
+            System.out.println("press 3 to speed up game");
+            System.out.println("press 4 to slow down game");
+            System.out.println("press 5 to go next");
+            System.out.println("press 0 to end game");
+            Scanner s1 = new Scanner(System.in);
+            x = s1.nextInt();
+            if (x == 1) {
+                System.out.println("saving...");
+            } else if (x == 2) {
+                System.out.println("loading....");
+
+
+            } else if (x == 3) {
+                System.out.println("speeding up");
+                if(y>=500)
+                    y = y-500;
+            } else if (x == 4) {
+                System.out.println("slowing down");
+                if(y<=5000)
+                    y=y+500;
+            } else if (x == 5) {
+                System.out.println("Next");
+            } else if (x == 0) {
+                System.out.println("Bye");
+                System.exit(0);
+            }
+
+            try {
+                Thread.sleep(y);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
